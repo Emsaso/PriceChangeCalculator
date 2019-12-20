9149ce53abba63f36e3fc458ab71be375ffc17f4
Emsaso <34371682+Emsaso@users.noreply.github.com>
2019-12-20 10:47:11 +0100

    x

--- Terje/InvestmentAnalyzer.cs
+++ Terje/InvestmentAnalyzer.cs
@@ -21,22 +21,32 @@ namespace PriceChangeCalculator.Terje
 
         public AnalysisResult Analyse()
         {
+            Console.WriteLine("Start-date: (02/01/1996 - 02/12/2019)");
+            var initDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("End-date: (02/01/1996 - 02/12/2019)");
+            var endDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Investment amount: (~10000)");
+            var investmentAmount = float.Parse(Console.ReadLine());
+            Console.WriteLine("Buy percentage: (~95)");
+            var buyPercentage = float.Parse(Console.ReadLine());
+            Console.WriteLine("Sell percentage: (~105)");
+            var sellPercentage = float.Parse(Console.ReadLine());
+            var trueStart = initDate.ToString("dd/MM/yyyy").Replace('.', '/');
+            var trueEnd = endDate.ToString("dd/MM/yyyy").Replace('.', '/');
             foreach (var valueAndDate in _list)
             {
-                //valueAndDate.
-
-
+                if (valueAndDate.Date < DateTime.Parse(trueStart) || valueAndDate.Date > DateTime.Parse(trueEnd)) continue;
                 var percentageUp = valueAndDate.Value / ValueWhenPurchased * 100;
                 var percentageDown = valueAndDate.Value / ValueWhenSold * 100;
-                var investmentAmount = 10000;
-                if (percentageDown < 100 && !StocksPurchased || !InitInvest)
+                //var investmentAmount = 10000;
+                if (percentageDown < buyPercentage && !StocksPurchased || !InitInvest)
                 {
                     InitInvest = true;
                     ValueWhenPurchased = valueAndDate.Value;
                     StocksPurchased = true;
                     AnalysisString = $"Date: {valueAndDate.Date:d} - Last: {valueAndDate.Value:0.00} - Investment: {investmentAmount}kr - Purchased at: {ValueWhenPurchased} ({percentageDown:0.00}%)";
                 }
-                else if (percentageUp > 105 && StocksPurchased)
+                else if (percentageUp > sellPercentage && StocksPurchased)
                 {
                     ValueWhenSold = valueAndDate.Value;
                     StocksPurchased = false;
@@ -46,7 +56,6 @@ namespace PriceChangeCalculator.Terje
                 {
                     AnalysisString = $"Date: {valueAndDate.Date:d} - Last: {valueAndDate.Value:0.00}";
                 }
-
                 StringBuild = new StringBuilder();
                 StringBuild.Append(AnalysisString);
                 Console.WriteLine(StringBuild);
